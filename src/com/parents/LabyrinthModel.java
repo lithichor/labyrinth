76a823fd99034698598097ae1082e52b49afa86e
Eric Hartill <euclidsieve@gmail.com>
2016-12-07 05:33:54 -0500

    Removed Hibernate code from LabyrinthModel and removed Hibernate config files

--- src/com/parents/LabyrinthModel.java
+++ src/com/parents/LabyrinthModel.java
@@ -2,13 +2,10 @@ package com.parents;
 
 import java.io.Serializable;
 
-import org.hibernate.HibernateException;
-
 import com.database.DatabaseHelper;
-import com.database.HibernateDBM;
 import com.labels.LabyrinthMessages;
 
-public abstract class LabyrinthModel extends HibernateDBM implements Serializable
+public abstract class LabyrinthModel implements Serializable
 {
 	private static final long serialVersionUID = 1L;
 	
@@ -20,58 +17,8 @@ public abstract class LabyrinthModel extends HibernateDBM implements Serializabl
 		dbh = DatabaseHelper.getInstance();		
 	}
 	
-	public boolean save() throws LabyrinthException
-	{
-		this.getSession();
-		boolean good = false;
-		
-		try
-		{
-			trans = session.beginTransaction();
-			session.saveOrUpdate(this);
-			trans.commit();
-			good = true;
-		}
-		catch(HibernateException he)
-		{
-			if(trans != null)
-			{
-				trans.rollback();
-			}
-			throw new LabyrinthException(he);
-		}
-		return good;
-	}
-	
 	protected DatabaseHelper getDbh()
 	{
 		return this.dbh;
-	}
-	
-	public boolean update() throws LabyrinthException
-	{
-		this.getSession();
-		boolean good = false;
-		
-		try
-		{
-			
-			// this isn't working. Look into these webpages:
-			// https://www.google.com/search?q=hibernate+saveorupdate&ie=utf-8&oe=utf-8#q=hibernate+change+fields+for+object
-			// http://www.journaldev.com/3481/hibernate-session-merge-vs-update-save-saveorupdate-persist-example
-			trans = session.beginTransaction();
-			session.save(this);
-			trans.commit();
-			good = true;
-		}
-		catch(HibernateException he)
-		{
-			if(trans != null)
-			{
-				trans.rollback();
-			}
-			throw new LabyrinthException(he);
-		}
-		return good;
 	}
 }
