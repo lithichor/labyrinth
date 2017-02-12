0334546be330d0cca94e6df59ce461162d41d5d3
Eric Hartill <euclidsieve@gmail.com>
2017-02-12 10:16:09 -0500

    #164 - changed endpoint in EndpointsWithIds

--- src/com/models/constants/EndpointsWithIds.java
+++ src/com/models/constants/EndpointsWithIds.java
@@ -3,7 +3,7 @@ package com.models.constants;
 public class EndpointsWithIds
 {
 	public static final String MAPS = "maps";
-	public static final String MAPS_GAMES = "maps/games";
+	public static final String MAPS_GAME = "maps/game";
 	public static final String TILES = "tiles";
 	public static final String TILES_MAPS = "tiles/map";
 	public static final String HEROS = "heros";
