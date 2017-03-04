3e4299563bcf559af106ebbe1985359c5bb838f4
Eric Hartill <euclidsieve@gmail.com>
2017-03-04 13:38:13 -0500

    #168 - added maxHealth to hero - updated db schema

--- src/com/web/api/hero/APIHero.java
+++ src/com/web/api/hero/APIHero.java
@@ -7,6 +7,7 @@ public class APIHero extends LabyrinthAPIModel
 	private Integer id;
 	private Integer gameId;
 	private Integer health;
+	private Integer maxHealth;
 	private Integer strength;
 	private Integer magic;
 	private Integer attack;
@@ -17,66 +18,27 @@ public class APIHero extends LabyrinthAPIModel
 		this.id = hero.getId();
 		this.gameId = hero.getGameId();
 		this.health = hero.getHealth();
+		this.maxHealth = hero.getMaxHealth();
 		this.strength = hero.getStrength();
 		this.magic = hero.getMagic();
 		this.attack = hero.getAttack();
 		this.defense = hero.getDefense();
 	}
 	
-	public Integer getId()
-	{
-		return id;
-	}
-	public void setId(Integer id)
-	{
-		this.id = id;
-	}
-	public Integer getGameId()
-	{
-		return gameId;
-	}
-	public void setGameId(Integer gameId)
-	{
-		this.gameId = gameId;
-	}
-	public Integer getHealth()
-	{
-		return health;
-	}
-	public void setHealth(Integer health)
-	{
-		this.health = health;
-	}
-	public Integer getStrength()
-	{
-		return strength;
-	}
-	public void setStrength(Integer strength)
-	{
-		this.strength = strength;
-	}
-	public Integer getMagic()
-	{
-		return magic;
-	}
-	public void setMagic(Integer magic)
-	{
-		this.magic = magic;
-	}
-	public Integer getAttack()
-	{
-		return attack;
-	}
-	public void setAttack(Integer attack)
-	{
-		this.attack = attack;
-	}
-	public Integer getDefense()
-	{
-		return defense;
-	}
-	public void setDefense(Integer defense)
-	{
-		this.defense = defense;
-	}
+	public Integer getId() { return id; }
+	public void setId(Integer id) { this.id = id; }
+	public Integer getGameId() { return gameId; }
+	public void setGameId(Integer gameId) { this.gameId = gameId; }
+	public Integer getHealth() { return health; }
+	public void setHealth(Integer health) { this.health = health; }
+	public Integer getMaxHealth() { return this.maxHealth; }
+	public void setMaxHealth(Integer maxHealth) { this.maxHealth = maxHealth; }
+	public Integer getStrength() { return strength; }
+	public void setStrength(Integer strength) { this.strength = strength; }
+	public Integer getMagic() { return magic; }
+	public void setMagic(Integer magic) { this.magic = magic; }
+	public Integer getAttack() { return attack; }
+	public void setAttack(Integer attack) { this.attack = attack; }
+	public Integer getDefense() { return defense; }
+	public void setDefense(Integer defense) { this.defense = defense; }
 }
