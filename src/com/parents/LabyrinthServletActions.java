0a9f6b2fcb46881c04fd8a3d34853a27cdb05b53
Eric Hartill <euclidsieve@gmail.com>
2017-03-05 13:37:31 -0500

    moved getGameId to map actions

--- src/com/parents/LabyrinthServletActions.java
+++ src/com/parents/LabyrinthServletActions.java
@@ -91,24 +91,4 @@ public class LabyrinthServletActions
 		}
 		return u;
 	}
-
-	public Integer getGameId(User user, JsonObject data) throws LabyrinthException
-	{
-		int gameId = 0;
-		
-		// if the gameId is specified, use that
-		if(data != null && data.has("gameId"))
-		{
-			try
-			{
-				gameId = data.get("gameId").getAsInt();
-			}
-			catch(NumberFormatException | IllegalStateException  | UnsupportedOperationException ex)
-			{
-				throw new LabyrinthException(messages.getMessage("map.gameId_not_integer"));
-			}
-		}
-		
-		return gameId;
-	}
 }
--- src/com/web/api/maps/MapsServletActions.java
+++ src/com/web/api/maps/MapsServletActions.java
@@ -1,7 +1,29 @@
 package com.web.api.maps;
 
+import com.google.gson.JsonObject;
+import com.parents.LabyrinthException;
 import com.parents.LabyrinthServletActions;
+import com.web.api.user.User;
 
 public class MapsServletActions extends LabyrinthServletActions
 {
+	public Integer getGameId(User user, JsonObject data) throws LabyrinthException
+	{
+		int gameId = 0;
+		
+		// if the gameId is specified, use that
+		if(data != null && data.has("gameId"))
+		{
+			try
+			{
+				gameId = data.get("gameId").getAsInt();
+			}
+			catch(NumberFormatException | IllegalStateException  | UnsupportedOperationException ex)
+			{
+				throw new LabyrinthException(messages.getMessage("map.gameId_not_integer"));
+			}
+		}
+		
+		return gameId;
+	}
 }
