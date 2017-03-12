6c9a65430a7e47a517d6d7526f8278cb454f9c49
Eric Hartill <euclidsieve@gmail.com>
2017-03-12 11:51:29 -0400

    #207 - added unit tests for combats put

--- src/com/parents/LabyrinthException.java
+++ src/com/parents/LabyrinthException.java
@@ -4,6 +4,11 @@ public class LabyrinthException extends Exception
 {
 	private static final long serialVersionUID = -1242191878331013412L;
 
+	public LabyrinthException()
+	{
+		super();
+	}
+	
 	public LabyrinthException(String message)
 	{
 		super(message);
