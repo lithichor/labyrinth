b8b5c6cd9813602a086d0bde0f76b2b51ce12aa8
Eric Hartill <erichartill@Erics-MacBook-Pro.local>
2017-03-27 10:02:25 -0700

    #223 - added unit tests for user -PUT

--- src/test/tests/user/RunAllUserTests.java
+++ src/test/tests/user/RunAllUserTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({UserServletPostTests.class, UserServletGetTests.class})
+@SuiteClasses({UserServletPostTests.class, UserServletGetTests.class, UserServletPutTests.class})
 public class RunAllUserTests
 {
 
