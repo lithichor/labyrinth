9f8f7531300032c9feb4c6b0c1698c0134ae5da7
Eric Hartill <erichartill@gmail.com>
2017-04-12 06:30:19 -0400

    #242 - added unit tests for tiles servlet get

--- src/test/tests/tiles/RunAllTilesTests.java
+++ src/test/tests/tiles/RunAllTilesTests.java
@@ -6,7 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	TilesMapGetTests.class
+	TilesMapServletGetTests.class,
+	TilesServletGetTests.class
 	})
 public class RunAllTilesTests
 {
